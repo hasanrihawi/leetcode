fec663443b9706255df75d35fb6e60d9d83961ca
Hasan Rihawi <Hasan.Rihawi@gbgplc.com>
2021-01-11 14:18:19 +0300

    add ZigzagConversionSolution

--- leetcode/Program.cs
+++ leetcode/Program.cs
@@ -6,7 +6,12 @@ namespace leetcode
     {
         static void Main(string[] args)
         {
-            var x = new LengthOfLongestSubstringSolution2().lengthOfLongestSubstring("au");
+           // var x = new LengthOfLongestSubstringSolution2().lengthOfLongestSubstring("au");
+
+
+            //new LongestPalindromeSolution().LongestPalindrome("au");
+            var res =  new ZigzagConversionSolution().Convert("PAYPALISHIRING", 3) == "PAHNAPLSIIGYIR";
+
         }
     }
 }
---
+++ leetcode/ZigzagConversionSolution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcode
+{
+    public class ZigzagConversionSolution
+    {
+        public string Convert(string s, int numRows)
+        {
+            List<StringBuilder> rows = new List<StringBuilder>();
+
+            int currRow = 0;
+            bool goingDown = false;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (currRow >= rows.Count)
+                    rows.Add(new StringBuilder());
+
+                rows[currRow].Append(s[i]);
+                if (currRow == 0 || currRow == numRows - 1)
+                    goingDown = !goingDown;
+
+                currRow += goingDown ? +1 : -1;
+            }
+
+            StringBuilder res = new StringBuilder();
+            foreach (var row in rows)
+                res.Append(row);
+            return res.ToString();
+        }
+    }
+
+}
