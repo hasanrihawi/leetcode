50ac39536902c3d70a182e1d8de08590e7ce503f
Hasan Rihawi <Hasan.Rihawi@gbgplc.com>
2021-06-17 11:39:21 +0300

    BinaryGap

---
+++ leetcode/BinaryGap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace leetcode
+{
+    public class BinaryGap
+    {
+        public int solution(int N)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            string binary = Convert.ToString(N, 2);
+            string pattern = @"(1)+(0{1,})(?=(1))";
+            Regex rg = new Regex(pattern);
+            MatchCollection matchedAuthors = rg.Matches(binary);
+            return matchedAuthors.Count > 0 ? matchedAuthors
+            .Cast<Match>()
+            .Max(x => x.Value.Where(c => c == '0').ToList().Count) : 0;
+        }
+    }
+}
