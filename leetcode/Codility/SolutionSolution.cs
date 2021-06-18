1e7b96cd0dfff04a2ce09baa198dc67146234ada
Hasan Rihawi <Hasan.Rihawi@gbgplc.com>
2021-06-18 10:34:08 +0300

    FrogJmp

---
+++ leetcode/Codility/SolutionSolution.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcode.Codility
+{
+    public class SolutionSolution
+    {
+        public int solution(int X, int Y, int D)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            return (int)Math.Ceiling((double)(Y - X) / (double)D);
+        }
+    }
+}
